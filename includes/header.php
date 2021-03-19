5f91b8bfe125623820443366ec9645d828ec58e1
jayb-1 <j_begay@outlook.com>
2021-03-19 00:02:54 -0400

    lab 7

--- includes/header.php
+++ includes/header.php
@@ -21,6 +21,8 @@ session_start();
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
+     <script src="https://kit.fontawesome.com/0809ee8fa6.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="../css/signup.css">
 
 </head>
